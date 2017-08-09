7029493ac86569772744b703d22c357c1041cf9f
BIT <BIT@DESKTOP-K00OCLP>
2017-08-09 10:09:39 +0900

    initcommit

---
+++ src/test/TCPServer.java
@@ -0,0 +1,70 @@
+package test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	private static final int SERVER_PORT = 5000;
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		try {
+			//1. 서버 소켓 생성
+			serverSocket = new ServerSocket();
+			//2. 바인딩(binding)
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String localHostAddress = inetAddress.getHostAddress();
+			
+			serverSocket.bind(new InetSocketAddress(localHostAddress, SERVER_PORT));
+			System.out.println("[server] binding " + localHostAddress + ":" + SERVER_PORT );
+			
+			//3. accept(연결 요청 기다림)
+			Socket socket = serverSocket.accept();
+			//연결전까지 대기 (블로킹)
+			
+			//4. 연결 성공
+			InetSocketAddress remoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			int remoteHostPort = remoteSocketAddress.getPort();
+			String remoteHostAddress = remoteSocketAddress.getAddress().getHostAddress();
+			System.out.println("[server] connected from " + remoteHostAddress + ":" + remoteHostPort);
+				try {
+					//5. I/O Stream 받아오기
+					InputStream is = socket.getInputStream();
+					OutputStream os = socket.getOutputStream();
+					
+					//6. 데이터 읽기(read)
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); //블락
+					if(readByteCount <= -1) {
+						System.out.println("[server] disconnection client");
+						//소켓에서 -1은 상대방에서 연결을 끊을때
+					}
+					String data = new String(buffer, 0, readByteCount,"UTF-8");
+					System.out.println("server received: " + data);
+					}catch(IOException e) {
+						e.printStackTrace();
+					}finally {
+						if(socket != null && serverSocket.isClosed() == false ) {
+							serverSocket.close();
+						}
+					}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}finally {
+			if(serverSocket != null && serverSocket.isClosed() == false) {
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
